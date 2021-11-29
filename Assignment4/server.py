7c14fe5caa45c980d729b9dd812641eba89527b0
Carditis <45385019+Carditis@users.noreply.github.com>
2021-11-29 19:01:07 +0100

    Update server.py

--- Assignment4/server.py
+++ Assignment4/server.py
@@ -1,6 +1,13 @@
 import machine
 import network
 import socket
+"""import time
+data = bytearray(2)
+i2c = machine.I2C(scl=machine.Pin(22),sda=machine.Pin(23))
+address = 24
+temp_reg = 5
+res_reg = 8
+tempu = 0.0"""
 
 ap = network.WLAN (network.AP_IF) #Creates access point. 
 #We create an instance of the class WLAN. The access point allows other clients to connect to the wifi
@@ -8,8 +15,16 @@ ap.active (True) #Activates network interface
 ap.config (essid = 'bestNet20') #essid = wifi access point name
 ap.config (authmode = 3, password = 'supersejtpass') #
 
-pins = [machine.Pin(i, machine.Pin.IN) for i in (16, 17, 21, 22, 23)] #we make the pins ready
+pins = [machine.Pin(i, machine.Pin.OUT) for i in (16, 17, 21)] #we make the pins ready
 
+#Reads temperature from SDA and SCL pins
+"""def converter(data):
+    value = (data[0] << 8) | data[1]
+    temp = (value & 0xFFF) / 16.0
+    if value & 0x1000:
+        temp -=256.0
+    return temp
+"""
 html = """<!DOCTYPE html>
 <html>
     <head> <title>ESP32 Pins</title> </head>
@@ -24,6 +39,25 @@ addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1] #return a list of arrays, we tak
 s = socket.socket() #creates a new socket
 s.bind(addr)        #binds the socket to a specific address
 s.listen(1)         #starts to listen on the socket
+
+"""def updaterFunc(address, temp_reg, data):
+    i2c.readfrom_mem_into(address, temp_reg, data)
+    tempu = converter(data)
+    print(tempu)
+    if tempu < 25:      #Lights up green led if temperature is below 25 degrees
+        pins[0].value(1)
+        pins[1].value(0)
+        pins[2].value(0)
+    elif tempu < 28:    #Lights up yellow led if 25 <= temperature < 28 
+        pins[0].value(0)
+        pins[1].value(1)
+        pins[2].value(0)
+    else:
+        pins[0].value(0)   #Lights up red led if temperature >= 28
+        pins[1].value(0)
+        pins[2].value(1)
+    time.sleep(1)
+"""
 
 print('listening on', addr)
 while True:
