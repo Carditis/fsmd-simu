150088175929af3967ab03f104b9179b4282c349
Carditis <45385019+Carditis@users.noreply.github.com>
2021-11-30 17:57:28 +0100

    small update

--- Assignment4/server.py
+++ Assignment4/server.py
@@ -1,75 +1,78 @@
 import machine
 import network
 import socket
-"""import time
+import time
+
 data = bytearray(2)
+input1 = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
 i2c = machine.I2C(scl=machine.Pin(22),sda=machine.Pin(23))
 address = 24
 temp_reg = 5
 res_reg = 8
-tempu = 0.0"""
-
-ap = network.WLAN (network.AP_IF) #Creates access point. 
-#We create an instance of the class WLAN. The access point allows other clients to connect to the wifi
-ap.active (True) #Activates network interface
-ap.config (essid = 'bestNet20') #essid = wifi access point name
-ap.config (authmode = 3, password = 'supersejtpass') #
-
-pins = [machine.Pin(i, machine.Pin.OUT) for i in (16, 17, 21)] #we make the pins ready
+tempu = 0.0
 
 #Reads temperature from SDA and SCL pins
-"""def converter(data):
+def converter(data):
     value = (data[0] << 8) | data[1]
     temp = (value & 0xFFF) / 16.0
     if value & 0x1000:
         temp -=256.0
     return temp
-"""
+
+ap = network.WLAN (network.AP_IF)
+ap.active (True)
+ap.config (essid = 'bestNet20')
+ap.config (authmode = 3, password = 'password')
+
+pins = [machine.Pin(i, machine.Pin.OUT) for i in (16, 17, 21)]
+
 html = """<!DOCTYPE html>
 <html>
     <head> <title>ESP32 Pins</title> </head>
     <body> <h1>ESP32 Pins</h1>
-        <table border="1"> <tr><th>Pind</th><th>Value</th></tr> %s </table>
+        <table border="1"> <tr><th>Variable</th><th>Value</th></tr> %s </table>
     </body>
 </html>
 """
 
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1] #return a list of arrays, we take the last element in the first array
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
-s = socket.socket() #creates a new socket
-s.bind(addr)        #binds the socket to a specific address
-s.listen(1)         #starts to listen on the socket
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
 
-"""def updaterFunc(address, temp_reg, data):
+print('listening on', addr)
+
+while True:
+    button = input1.value()
     i2c.readfrom_mem_into(address, temp_reg, data)
     tempu = converter(data)
     print(tempu)
-    if tempu < 25:      #Lights up green led if temperature is below 25 degrees
+    if tempu < 25:
         pins[0].value(1)
         pins[1].value(0)
         pins[2].value(0)
-    elif tempu < 28:    #Lights up yellow led if 25 <= temperature < 28 
+    elif tempu < 28:
         pins[0].value(0)
         pins[1].value(1)
         pins[2].value(0)
     else:
-        pins[0].value(0)   #Lights up red led if temperature >= 28
+        pins[0].value(0)
         pins[1].value(0)
         pins[2].value(1)
-    time.sleep(1)
-"""
-
-print('listening on', addr)
-while True:
-    cl, addr = s.accept()   #now we start to accept connections
+        
+    cl, addr = s.accept()
     print('client connected from', addr)
     cl_file = cl.makefile('rwb', 0)
     while True:
         line = cl_file.readline()
-        print(line)
+        #print(line)
         if not line or line == b'\r\n':
             break
-    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]      #the pin values are read and updates the array
-    response = html % '\n'.join(rows)                                                   #the array is concatenated with the html from line 13
-    cl.send(response)                                                                   #now we send it
+    rows1 = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+    rows2 = ['<tr><td>%s</td><td>%d</td></tr>' % ("Temperature", tempu)]
+    rows3 = ['<tr><td>%s</td><td>%d</td></tr>' % ("Button state", button)]
+    rows = rows1 + rows2 + rows3
+    response = html % '\n'.join(rows)
+    cl.send(response)
     cl.close()
